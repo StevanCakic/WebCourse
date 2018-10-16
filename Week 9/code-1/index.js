6c83acce3811517ea5dc04ab2e506b3b272e973a
StevanCakic <stevancakic93@gmail.com>
2018-10-16 14:37:44 +0200

    code for class 20 attached

---
+++ Week 9/code-1/index.js
@@ -0,0 +1,16 @@
+function waitTreeSeconds(){
+    var ms = 3000 + new Date().getTime();
+    while((new Date()).getTime() < ms){
+        
+    }
+
+    console.log("Finished function");
+}
+
+function clickHandler(){
+    console.log('click event');
+}
+
+document.addEventListener("click",clickHandler);
+waitTreeSeconds();
+console.log("finished execution");
