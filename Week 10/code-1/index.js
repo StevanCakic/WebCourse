cf161bafddc1937f9814cdb248319e3fa2922a42
StevanCakic <stevancakic93@gmail.com>
2018-10-21 17:58:11 +0200

    attach some code for 21. class

---
+++ Week 10/code-1/index.js
@@ -0,0 +1,20 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
+
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.addEventListener("readystatechange", function () {
+        if (this.readyState == 4 && this.status == 200) {
+            $("#demo").innerHTML = this.responseText;
+        }
+    })
+    xhttp.open("GET", "demo_get.txt", true);
+    xhttp.send();
+}
+
+$("#reqData").addEventListener("click", loadDoc);
---
+++ Week 10/code-2/index.js
@@ -0,0 +1,59 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function myFunction1(xhttp) {
+    console.log(xhttp.getAllResponseHeaders());
+    $("#demo").innerHTML =
+        xhttp.responseText;
+}
+
+function myFunction2(xhttp) {
+    let xmlDoc = xhttp.responseXML;
+    let txt = "";
+    let x = xmlDoc.getElementsByTagName("ARTIST");
+    for (let i = 0; i < x.length; i++) {
+        txt = txt + x[i].childNodes[0].nodeValue + "<br>";
+    }
+    console.log(xhttp.getResponseHeader("last-modified"));
+    $("#demo").innerHTML = txt;
+}
+
+function createTable (xhttp) {
+
+    let xmlDoc = xhttp.responseXML;
+    let table = "<table><tr><th>Artist</th><th>Title</th></tr>";
+    let x = xmlDoc.getElementsByTagName("CD");
+    for (let i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+            x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+            "</td></tr>";
+    }
+    $("#demo").innerHTML = table + "</table>";
+}
+
+$("#htmlContent").addEventListener("click", function () {
+    loadDoc("ajax_info.txt", myFunction1);
+})
+
+$("#xmlContent").addEventListener("click", function () {
+    loadDoc("cd_catalog.xml", myFunction2);
+})
+
+$("#xmlTable").addEventListener("click", function () {
+    loadDoc("cd_catalog.xml", createTable);
+})
