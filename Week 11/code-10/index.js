6612cf63c8c66286904aa8309cfddb5f6f6b49a8
StevanCakic <stevancakic93@gmail.com>
2018-10-31 23:14:01 +0100

    code for class 24 attached

---
+++ Week 11/code-10/index.js
@@ -0,0 +1,23 @@
+const url = "https://api.github.com/users";
+
+function checkStatus(response) {
+    if (response.status === 200) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+
+function getJSON(response) {
+    return response.json();
+}
+
+fetch(url)
+    .then(checkStatus)
+    .then(getJSON)
+    .then(function (data) {
+        console.log("Data ", data);
+    })
+    .catch(function (err) {
+        console.log("Error ", err);
+    })
